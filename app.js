0a15e0b8ba6619bb96959955f1d1119d65e3ad4e
abdomohammed2002 <3bdomo7ammed2002@gmail.com>
2024-03-11 07:29:13 +0200

    modifing courses controller

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const path = require("path");
 var cors = require("cors");
 require("dotenv").config();
 
-const url = process.env.MONGO_URL;
+const url =
+  "mongodb+srv://abdo:nodejs-123@courses.z4fthcw.mongodb.net/node-course?retryWrites=true&w=majority&appName=courses";
 const main = async () => {
   await mongoose.connect(url);
   console.log("mongoose connect");
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use("/api/courses", coursesRouter);
 app.use("/api/users", usersRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(cors());
+// app.use(cors());
 app.use((error, req, res, next) => {
   res
     .status(error.statusCode)
--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -1,36 +1,79 @@
 const Course = require("../models/course.model");
-const httpStatusText = require("../utils/httpStatusText");
 const asyncWrapper = require("../middleware/asyncWrapper");
+const httpStatusText = require("../utils/httpStatusText");
 const appErrors = require("../utils/appErrors");
 
 const getAllCourses = asyncWrapper(async (req, res, next) => {
-  // const { limit =4, skip=2 } = req.query;
-  const courses = await Course.find({}, { __v: false });
-  // .limit(limit).skip(skip);
+  const { limit = 2, page = 1 } = req.query;
+  const skip = (page - 1) * limit;
+  const courses = await Course.find({}, { __v: false }).limit(limit).skip(skip);
   res.status(200).json({ status: httpStatusText.SUCCESS, data: { courses } });
 });
+
 const getOneCourse = asyncWrapper(async (req, res, next) => {
-  const courseId = req.params.courseId;
-  const course = await Course.findById(courseId);
+  const course = await Course.findById(req.params.courseId);
   if (!course) {
     const error = appErrors.create(
-      404,
+      400,
       httpStatusText.FAIL,
       "course not found"
     );
     return next(error);
   }
-  res.json({ status: httpStatusText.SUCCESS, data: { course } });
-  next();
+  res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
 });
-const addOneCourse = async () => {};
-const updateOneCourse = async () => {};
-const deleteOneCourse = async () => {};
+
+const updateOneCourse = asyncWrapper(async (req, res, next) => {
+  const course = await Course.findOne(
+    { _id: req.params.courseId },
+    { __v: false }
+  );
+  if (!course) {
+    const error = appErrors.create(
+      400,
+      httpStatusText.FAIL,
+      "course not found"
+    );
+    return next(error);
+  }
+  const updatedCourse = await Course.updateOne(
+    { _id: req.params.courseId },
+    { $set: req.body }
+  );
+  res
+    .status(200)
+    .json({ status: httpStatusText.SUCCESS, data: { course: updatedCourse } });
+});
+
+const addOneCourse = asyncWrapper(async (req, res, next) => {
+  const newCourse = new Course(req.body);
+  await newCourse.save();
+  res
+    .status(201)
+    .json({ status: httpStatusText.SUCCESS, data: { course: newCourse } });
+});
+
+const deleteOneCourse = asyncWrapper(async (req, res, next) => {
+  const course = await Course.findByIdAndDelete(req.params.courseId);
+  if (!course) {
+    const error = appErrors.create(
+      400,
+      httpStatusText.FAIL,
+      "course not found"
+    );
+    return next(error);
+  }
+  res.json({
+    status: httpStatusText.SUCCESS,
+    data: { course: null },
+    message: "course deleted",
+  });
+});
 
 module.exports = {
   getAllCourses,
   getOneCourse,
-  addOneCourse,
   updateOneCourse,
+  addOneCourse,
   deleteOneCourse,
 };
